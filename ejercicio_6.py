acaf4754ba7daeac409939193982f2dcdbf5c10f
dafloresdiaz <dafloresdiaz@gmail.com>
2020-10-19 21:35:33 -0500

    Unit test for exercise 6
    Created unit test for the exercise 6 to prove id the functions is working as expected

--- ejercicio_6.py
+++ ejercicio_6.py
@@ -4,13 +4,9 @@ def totalOverlaps(intervals):
     count = 0
     for i in range(n):
         for y in range(i+1, n): 
-            if intervals[i][0] == intervals[y][0] or intervals[i][1] > intervals[y][0]:
+            if intervals[i][0] == intervals[y][0]:
                 count += 1
             else:
                 pass
     return count
-
-
-listIntervals = [[1,2],[1,2],[1,2]]
-print(totalOverlaps(listIntervals))
 
--- unitTest_ejercicio_6.py
+++ unitTest_ejercicio_6.py
@@ -1,3 +1,16 @@
 from ejercicio_6 import totalOverlaps as TL
 
-def test
+def testOverLap1():
+    listOfIntervals = [[1,2],[2,3],[3,5]]
+    result = TL(listOfIntervals)
+    assert result == 0
+
+def testOverlap2():
+    listOfIntervals = [[7,9], [2,4], [5,8]]
+    result = TL(listOfIntervals)
+    assert result == 0
+
+def testOverlap3():
+    listOfIntervals = [[1,2], [2,3], [3,4], [1,3]]
+    result = TL(listOfIntervals)
+    assert result == 1
