2eb7bfeea71cf0b5bac9017a58bbaf6423172017
dafloresdiaz <dafloresdiaz@gmail.com>
2020-10-24 00:50:26 -0500

    Finished the exercise and the code
    The code is done, but is necessary to do more unit test to prove that is working correctly and do some modifications to the conditions

--- ejercicio_12.py
+++ ejercicio_12.py
@@ -1,10 +1,18 @@
 def stringIsomorphic(s1, s2):
     dic = {} 
+    count = 0
     sameSize = len(s1) == len(s2)
     if sameSize:
         for i in range(len(s1)):
-            dic[s1[i]] = s2[i]
-    print(dic)
+            if s1[i] in dic:
+                if dic.get(s1[i]) != s2[i]:
+                    count += 1
+            else:
+                dic[s1[i]] = s2[i]
+    if count > 0:
+        return False
+    else:
+        return True
 
-stringIsomorphic("abc", "bbc")
+print(stringIsomorphic("aac", "bcc"))
 
