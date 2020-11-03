5d17a72495f0d37098674ca82bfc742c1f2b23c6
dafloresdiaz <dafloresdiaz@gmail.com>
2020-11-02 20:52:22 -0600

    Added more test case for the exercise 18

--- unitTest_ejercicio_18.py
+++ unitTest_ejercicio_18.py
@@ -4,3 +4,8 @@ def testMajority1():
     listElements = [1, 2, 1, 1, 3, 4, 0]
     result = majorityOfElements(listElements)
     assert result == 1
+
+def testMajority2():
+    listElements = [3,4,52,1,4,1,4,21,4,6,4]
+    result = majorityOfElements(listElements)
+    assert result == 4
